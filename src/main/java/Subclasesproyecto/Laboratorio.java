140762de9574ecbfc6b6f735b470484933de36a7
Usuario Toshiba <80179255+emelysantosmejia@users.noreply.github.com>
2021-03-11 17:12:08 -0600

    Primer commit para repositorio remmoto -

---
+++ src/main/java/Subclasesproyecto/Laboratorio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Subclasesproyecto;
+
+/**
+ *
+ * @author Emely  Santos
+ */
+public class Laboratorio extends Herencia {
+   
+    public Laboratorio(){
+        establecerEdificio("200 metros cuadrados");
+        establecerPisos("6 pisos");
+                
+    }
+}
